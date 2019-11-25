9c4382187a3e583a2aef08e3f476d18d3d39d193
Bailey Russo (RIT Student) <bar3371@ad.rit.edu>
2019-11-25 14:41:01 -0500

    Pop-Ups Working

--- Testingone.js
+++ Testingone.js
@@ -5,7 +5,8 @@ var view;
 var populationChange;
 
 
-require(["esri/tasks/Locator", "esri/Map", "esri/views/MapView","esri/layers/FeatureLayer"], function(Location, Map, MapView,FeatureLayer) {
+require(["esri/tasks/Locator", "esri/Map", "esri/views/MapView","esri/layers/FeatureLayer"], 
+function(Location, Map, MapView,FeatureLayer) {
     
 
     map = new Map({
@@ -20,48 +21,21 @@ require(["esri/tasks/Locator", "esri/Map", "esri/views/MapView","esri/layers/Fea
         
         	  //<!-- add mouse handler after map constructor -->
     //view.on("pointer-move", executeMouseMove);
+   
+   
+    var popupTrailheads = {
+      "title": "{STATE_NAME}",
+      "content": "Popultation: <b>{POP2010}</b> <br>Men: {MALES} <br>Females: {FEMALES}"
+    }
     var FeatureLayer = new FeatureLayer({
-        url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_States_Generalized/FeatureServer/"
-    //url:"http://services.arcgis.com/JbibFpsuaEQr3FFG/arcgis/rest/services/State_to_State_Migration/FeatureServer/",
-  
+        url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_States_Generalized/FeatureServer/",
+        outFields: ["STATE_NAME"],
+        popupTemplate: popupTrailheads
     });//end of feature layer
-
-    
-
-    view.popup.autoOpenEnabled = false;
-view.on("click", function(event) {
-  // Get the coordinates of the click on the view
-  // around the decimals to 3 decimals
-  var lat = Math.round(event.mapPoint.latitude * 1000) / 1000;
-  var lon = Math.round(event.mapPoint.longitude * 1000) / 1000;
-
-  view.popup.open({
-    // Set the popup's title to the coordinates of the clicked location
-    title: "{STATE_NAME}",
-    location: event.mapPoint // Set the location of the popup to the clicked location
-  });
-});
 
     map.zoom = FeatureLayer.zoom;
     //view.zoom = 10;O
     
-    map.add(FeatureLayer);
-
-    var popupTemplate = {
-        // autocasts as new PopupTemplate()
-        title: "Population in {NAME}",
-        outFields: ["*"],
-        content: populationChange,
-        fieldInfos: [
-          {
-            fieldName: "POP2010",
-            format: {
-              digitSeparator: true,
-              places: 0
-            }
-          }]
-        };
-
-    //FeatureLayer.popupTemplate = popupTemplate;
+    map.add(FeatureLayer,0);
 
 });//end of require
