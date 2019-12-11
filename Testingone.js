2fd98a1b373ade21821c727cc802b011714c3f4a
Bailey Russo (RIT Student) <bar3371@ad.rit.edu>
2019-12-10 19:48:40 -0500

    User ability

--- Testingone.js
+++ Testingone.js
@@ -11,53 +11,56 @@ ESRI Maps
 All Right Reserved
 */
 
-var map;
-var view;
+var map;//global map
+var view;//
 var searchWidget;
 var currentURL;
+var FeatureLayer;
 
+//required fields to complete the necessary tasks
 require(["esri/tasks/Locator", "esri/Map", "esri/views/MapView","esri/layers/FeatureLayer","esri/widgets/Search","esri/symbols/SimpleFillSymbol","esri/Color"], 
 function(Location, Map, MapView,FeatureLayer, Search,SimpleFillSymbol, Color) {
     
-
     map = new Map({
-      basemap: "osm"
+      basemap: "osm"//basemap
     });
     view = new MapView({
-		container: "viewDiv", // Reference to the DOM node that will contain the view
+		container: "viewDiv",//gets div container 
 		map: map,
-		center: [-98.5795, 39.8283],
-		zoom: 4.7
+		center: [-98.5795, 39.8283],//center of U.S.
+		zoom: 4.7//sets zoom
         });
-        
-        	  //<!-- add mouse handler after map constructor -->
-   // view.on("pointer-move", executeMouseMove);
 
     var popupTrailheads = {
-      "title": "{NAME} County",
-      "content": "Popultation: <b>{POP2010}</b> <br>Men: {MALES} <br>Females: {FEMALES}"
-    }
-    var FeatureLayer = new FeatureLayer({
+      title: "{NAME}",//title is state
+      content: "<b>{WINNER}</b> <br> Trump: {VOTE_TR} <br> Clinton: {VOTE_CL}"
+      
+    }//end of popupTrails
+
+//https://developers.arcgis.com/javascript/latest/sample-code/popuptemplate-function/index.html
+//https://www.arcgis.com/home/webmap/viewer.html?layers=66e3efa3bd0f4b5496228a4328930c89
+    FeatureLayer = new FeatureLayer({
         //url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_States_Generalized/FeatureServer/",
-        url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Counties_Generalized/FeatureServer",
-        outFields: ["STATE_NAME"],
+        //url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Counties_Generalized/FeatureServer",
+        url: "https://services1.arcgis.com/VAI453sU9tG9rSmh/arcgis/rest/services/Maps101_Election_Center_2016_features/FeatureServer/2",
+
         popupTemplate: popupTrailheads
     });//end of feature layer
 
-    //map.zoom = FeatureLayer.zoom;
-    //view.zoom = 10;O
-    map.add(FeatureLayer,0);
+    map.add(FeatureLayer,0);//adds to map
 
+    //instantiates searchbar
     searchWidget = new Search({
       view: view
     }),
 
+    //adds search widget to top right
     view.ui.add(searchWidget,{
       position: "top-right",
       index: 2
     });
     
-
+    //on load push fields
     searchWidget.on("load", function(){
       
       var sources = searchWidget.sources;
@@ -65,19 +68,16 @@ function(Location, Map, MapView,FeatureLayer, Search,SimpleFillSymbol, Color) {
         searchFields: ["County"],
         displayField: "County",
   
-
       });
       searchWidget.set("sources",sources);
     });
 
-    
+    //search for county or state
     searchWidget.watch('activeSource', function(evt){
       evt.placeholder = "Search County or State";
     });
 
-    searchWidget.startup();
-
-   //window.onload = ChangeMap();
+    searchWidget.startup();//sets up searchwidget
 
 });//end of require
 
@@ -107,18 +107,18 @@ function(Location, Map, MapView,FeatureLayer, Search,SimpleFillSymbol, Color) {
           "content": "Popultation: <b>{POP2010}</b> <br>Men: {MALES} <br>Females: {FEMALES}"
         }
 
-      }
+      }//end of ifelse
+
 
       var FeatureLayer = new FeatureLayer({
-        url: URL_root,
+        url: URL_root,//gets the URL
         outFields: ["STATE_NAME"],
-        popupTemplate: popupTrailheads
+        popupTemplate: popupTrailheads//SETS pop up data
       });
 
-      map.removeAll();
+      map.removeAll();//gets rid of all layers
 
-      map.layers.add(FeatureLayer,1);
+      map.layers.add(FeatureLayer,1);//adds layer to map
     
-      //setLayerGlobalVisibility(1, FeatureLayer);
       });
 }//end of ChangeMap
